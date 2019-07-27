bac107f29dd6f075fd288db2555716ff784e38c9
nguyendinhphuc <phucdinhnguyen09052000@gmail.com>
2019-07-27 03:30:57 -0700

    cap nhat file anh & lam carousel

--- src/app/entities/home/trang-chu/trang-chu.module.ts
+++ src/app/entities/home/trang-chu/trang-chu.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TrangChuComponent } from "./trang-chu.component";
 import { TrangChuRoutingModule } from "./trang-chu-routing.module";
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
-  declarations: [TrangChuComponent],
+  declarations: [TrangChuComponent, CarouselComponent],
   imports: [CommonModule, TrangChuRoutingModule]
 })
 export class TrangChuModule {}
