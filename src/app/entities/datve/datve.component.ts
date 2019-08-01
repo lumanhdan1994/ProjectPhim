738e6e8c89764afa55bfbfa5cd5c9d45f5d1d1cc
nguyendinhphuc <phucdinhnguyen09052000@gmail.com>
2019-08-01 08:01:40 -0700

    update datve

--- src/app/entities/datve/datve.component.ts
+++ src/app/entities/datve/datve.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { DataService } from 'src/app/shared/services/data.service';
+import { ItemgheComponent } from './itemghe/itemghe.component';
 
 declare var $: any;
 
@@ -9,7 +10,7 @@ declare var $: any;
   styleUrls: ['./datve.component.scss']
 })
 export class DatveComponent implements OnInit {
-
+  @ViewChildren(ItemgheComponent) tagItemGhe: QueryList<ItemgheComponent>;
   thongTinPhim: any;
   thongTinSuatChieu: any;
   soLuongVe: number = 0;
@@ -20,6 +21,9 @@ export class DatveComponent implements OnInit {
   tenGheDaChon: any;
   styleGheDaChon: boolean = false;
   mangGheDaChon: any = [];
+  navtag1: boolean = true;
+  navtag2: boolean = true;
+  navtag3: boolean = true;
 
   constructor(private dataService: DataService) { }
 
@@ -63,23 +67,39 @@ export class DatveComponent implements OnInit {
     $("#navtagChonGhe").click();
   }
 
-  chonGhe($event){
-    if($event.trangThaiChon === true){
-      this.mangGheDaChon.push($event);
-    }else{
-      this.mangGheDaChon.map((item,index) =>{
-        if(item.ghe.tenGhe === $event.ghe.tenGhe){
-          this.mangGheDaChon.splice(index,1);
+  toChonVe(){
+    $("#navtagChonVe").click();
+  }
+
+  chonGhe($event) {
+    if ($event.trangThaiChon === true) {
+      if (this.mangGheDaChon.length < this.soLuongVe) {
+        this.mangGheDaChon.push($event);
+      } else {
+        alert(`Hey! bạn chỉ được chọn ${this.soLuongVe} ghế thôi`);
+        this.tagItemGhe.map((item) => {
+          if (item.ghe.maGhe === $event.ghe.maGhe) {
+            item.styleGheDaChon = !item.styleGheDaChon;
+          }
+        })
+      }
+    } 
+
+
+    if($event.trangThaiChon === false) {
+      this.mangGheDaChon.map((item, index) => {
+        if (item.ghe.tenGhe === $event.ghe.tenGhe) {
+          this.mangGheDaChon.splice(index, 1);
         }
       })
     }
   }
 
+
   layThongTinSuatChieu(){
     const uri = "QuanLyDatVe/LayDanhSachPhongVe?MaLichChieu=16099";
     this.dataService.get(uri).subscribe((data:any) => {
       this.thongTinSuatChieu = data;   
-      console.log(this.thongTinSuatChieu);
     })
   }
 
--- src/app/entities/datve/itemghe/itemghe.component.ts
+++ src/app/entities/datve/itemghe/itemghe.component.ts
@@ -18,6 +18,7 @@ export class ItemgheComponent implements OnInit {
   }
 
   chonGhe() {
+    console.log(this.ghe);
     this.styleGheDaChon = !this.styleGheDaChon;
     const objGhe = {
       trangThaiChon: this.styleGheDaChon,
