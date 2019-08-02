a5fb1bf2ea35be6f00bfef5b48f0ca82e7f95540
lumanhdan1994 <lumanhdan1994@gmail.com>
2019-08-02 10:23:17 +0700

    error laythongtinrap()

--- src/app/entities/home/list-movie/list-movie.component.ts
+++ src/app/entities/home/list-movie/list-movie.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { DataService } from "./../../../shared/services/data.service";
 import { Subscription } from "rxjs";
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from "@angular/router";
+declare var $: any;
 
 @Component({
   selector: "app-list-movie",
@@ -12,29 +13,46 @@ export class ListMovieComponent implements OnInit {
   subListMovie: Subscription;
   maPhim: any;
   phimDetail: any;
-  constructor(private dataService: DataService, private activatedRoute: ActivatedRoute) { }
+  lichChieu: any[] = [];
+  thongTinHeThongRap: any;
+  maHeThongRap: any;
+  constructor(
+    private dataService: DataService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.getParamsUrl();
     this.getDetailMovie();
+    this.LayThongTinRap();
+    // console.log(this.lichChieu);
   }
 
   getParamsUrl() {
     this.maPhim = this.activatedRoute.snapshot.paramMap.get("malichchieu");
-    console.log(this.maPhim);
+    // console.log(this.maPhim);
   }
 
-  getDetailMovie(){
+  getDetailMovie() {
     const uri = `QuanLyPhim/LayThongTinPhim?MaPhim=${this.maPhim}`;
     this.dataService.get(uri).subscribe((data: any) => {
       this.phimDetail = data;
-      console.log(data);
+      // console.log(this.phimDetail);
+      this.phimDetail.lichChieu.map(item => {
+        this.lichChieu.push(item);
+      });
     });
   }
 
-
-
-
+  LayThongTinRap() {
+    console.log(this.lichChieu);
+    this.lichChieu.map(item => {
+      console.log(item);
+    });
+    const uri = `QuanLyRap/LayThongTinHeThongRap?maHeThongRap=${
+      this.maHeThongRap
+    }`;
+  }
 
   // getListMovie() {
   //   const uri = "QuanLyPhim/LayDanhSachPhim?maNhom=GP01";
