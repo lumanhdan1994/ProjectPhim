f31e31c6ae85db15dea5d30250eeb989a36651d2
nguyendinhphuc <phucdinhnguyen09052000@gmail.com>
2019-08-18 00:07:37 -0700

    done datve

--- src/app/entities/datve/datve.component.ts
+++ src/app/entities/datve/datve.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { DataService } from 'src/app/shared/services/data.service';
 import { ItemgheComponent } from './itemghe/itemghe.component';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
 import { FormComponent } from './form/form.component';
 
 
@@ -279,9 +279,6 @@ export class DatveComponent implements OnInit {
             alert("Vui lòng điền đầy đủ thông tin!");
           } else {
             const uri = "QuanLyDatVe/DatVe";
-
-
-
             let mangThongTinVe = [];
 
             this.mangGheDaChon.map((item) => {
@@ -299,7 +296,7 @@ export class DatveComponent implements OnInit {
               };
               mangThongTinVe = [...mangThongTinVe, thongTinVe]
             })
-            if( this.taiKhoanDaDangNhap === null ){
+            if (this.taiKhoanDaDangNhap === null) {
               this.taiKhoanDaDangNhap = JSON.parse(localStorage.getItem("taiKhoanDaDangNhap"));
             }
             let thongTinDatVe = {
@@ -309,10 +306,17 @@ export class DatveComponent implements OnInit {
             };
             this.dataService.post(uri, thongTinDatVe).subscribe(
               (data) => {
-                let datVeThanhCongInfor = {
-                  
-                };
-                this.router.navigate(['/datvethanhcong', datVeThanhCongInfor]);
+
+              }, (err) => {
+                this.router.navigate(['/datvethanhcong'], {
+                  queryParams: { 
+                    tongTien: this.tongTien,
+                    maLichChieu: this.maLichChieu,
+                    maPhim: this.maPhim,
+                    soLuongCombo: this.soLuongCombo,
+                    ghe: this.mangTenGheDuocClick,
+                   },
+                })
               }
             )
           }
--- src/app/entities/home/dat-ve-thanh-cong/dat-ve-thanh-cong.component.ts
+++ src/app/entities/home/dat-ve-thanh-cong/dat-ve-thanh-cong.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
   selector: 'app-dat-ve-thanh-cong',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatVeThanhCongComponent implements OnInit {
 
-  constructor() { }
+  inforTicket: any = {};
+  thongTinSuatChieu: any = {};
+  maPhim: any;
+  thongTinPhim: any = {};
+
+  constructor(private dataService: DataService, private activatedRoute: ActivatedRoute ) { }
 
   ngOnInit() {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      this.inforTicket = params;
+    });
+    this.layThongTinSuatChieu();
+    
   }
+
+  layThongTinSuatChieu() {
+    const uri = `QuanLyDatVe/LayDanhSachPhongVe?MaLichChieu=${this.inforTicket.maLichChieu}`;
+    this.dataService.get(uri).subscribe((data: any) => {
+      this.thongTinSuatChieu = data;
+      this.layThongTinChiTietPhim();
+    })
+  }
+
+  layThongTinChiTietPhim() {
+    const uri = "QuanLyPhim/LayDanhSachPhim?maNhom=GP08";
+    this.dataService.get(uri).subscribe((data: any) => {
+      data.map(item => {
+        if (item.tenPhim === this.thongTinSuatChieu.tenPhim) {
+          this.maPhim = item.maPhim;
+        }
+      })
+      const uri = `QuanLyPhim/LayThongTinPhim?MaPhim=${this.maPhim}`;
+      this.dataService.get(uri).subscribe((data: any) => {
+        this.thongTinPhim = data;
+      });
+    });
+  }
+
 
 }
