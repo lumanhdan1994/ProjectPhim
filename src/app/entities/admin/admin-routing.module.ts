954755e8021b65ea064874e40ad74e4af9a103f9
lumanhdan1994 <lumanhdan1994@gmail.com>
2019-08-22 20:12:17 +0700

    Admin Protection

--- src/app/entities/admin/admin-routing.module.ts
+++ src/app/entities/admin/admin-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AdminComponent } from "./admin.component";
+import { QuanLyUserComponent } from './quan-ly-user/quan-ly-user.component';
+import { AuthGuard } from 'src/app/common/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: "",
-    component: AdminComponent
+    component: AdminComponent, canActivate: [AuthGuard]
   }
 ];
 
